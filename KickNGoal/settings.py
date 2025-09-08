2d2de074b6f3128d91b07e031ca7a3f7a0ecbf43
randrasyah <randra.farasya@gmail.com>
2025-09-09 00:27:17 +0700

    Debug kesalahan settings.py

--- KickNGoal/settings.py
+++ KickNGoal/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fairuz-akhtar-KickNGoal.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fairuz-akhtar-kickngoal.pbp.cs.ui.ac.id"]
 
 
 # Application definition
